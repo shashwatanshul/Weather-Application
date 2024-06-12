2106b84602831ad23cd95103eac29bc2e92a4a5c
shashwatanshul <shashwatanshul@gmail.com>
2024-06-12 21:01:45 +0530

    1st commit
    1st commit

---
+++ src/Utils/useDate.jsx
@@ -0,0 +1,69 @@
+// import { useEffect, useState } from "react";
+
+// export const useDate = () => {
+//   const locale = "en";
+//   const [today, setDate] = useState(new Date());
+
+//   useEffect(() => {
+//     const timer = setInterval(() => {
+//       setDate(new Date());
+//     }, 1000);
+
+//     return () => {
+//       clearInterval(timer);
+//     };
+//   }, []);
+
+//   const day = today.toLocaleDateString(locale, { weekday: "long" });
+//   const date = `${day}, ${today.getDate()}, ${today.toLocaleDateString(locale, {
+//     month: "long",
+//   })}\n\n`;
+//   const time = today.toLocaleDateString(locale, {
+//     hour: "numeric",
+//     hour12: true,
+//     minute: "numeric",
+//     second: "numeric",
+//   });
+
+//   console.log("Time : ", time);
+
+//   return {
+//     date,
+//     time,
+//   };
+// };
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+import { useEffect, useState } from "react";
+
+export const useDate = () => {
+  const locale = "en";
+  const [today, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  const day = today.toLocaleDateString(locale, { weekday: "long" });
+  const date = `${day}, ${today.getDate()}, ${today.toLocaleDateString(locale, {
+    month: "long",
+  })}\n\n`;
+  const time = today.toLocaleTimeString(locale, {
+    hour: "numeric",
+    hour12: true,
+    minute: "numeric",
+    second: "numeric",
+  });
+
+  console.log("Time : ", time);
+
+  return {
+    date,
+    time,
+  };
+};
